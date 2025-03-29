f6be0d5d130365c11c6d8831ebea97b4b5dc0f0a
TomiSach <tomassachini@hotmail.com>
2025-03-29 17:18:18 -0300

    ready to depley

--- shipping_calculator.c
+++ shipping_calculator.c
@@ -1,14 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h> // Para exit()
+
 #define COMMON_5KG 500
 #define PREF_5KG 400
 #define ENTERPRISE_20KG 1500
 
-void shippingCalculator();
-
-char getUserChoice();
-
 void shippingCalculator()
 {
-    char choice[1];
+    char choice;
     float weight = 0;
     int opt;
     float overWeight = 0;
@@ -20,64 +19,72 @@ void shippingCalculator()
         printf("\n Client types:\n 1) Common customer\n 2) Preference customer \n 3) Enterprise customer\n 4) Exit\n");
         printf("\nSelect an option: ");
         scanf("%d", &opt);
+
+        if (opt == 4)
+        {
+            printf("Exiting...\n");
+            exit(0);
+        }
+
         printf("Enter package weight: ");
         scanf("%f", &weight);
 
         switch (opt)
         {
-
         case 1:
-
             if (weight >= 5)
             {
                 overWeight = weight - 5;
-
                 price = COMMON_5KG + (overWeight * 100);
             }
             else
             {
                 price = COMMON_5KG;
             }
+            printf("El total a pagar es de: %.2f\n", price);
             break;
 
         case 2:
             if (weight >= 5)
             {
                 overWeight = weight - 5;
-
                 price = PREF_5KG + (overWeight * 100);
             }
             else
             {
                 price = PREF_5KG;
             }
-
+            printf("El total a pagar es de: %.2f\n", price);
             break;
 
         case 3:
-            if (weight >= 5)
+            if (weight >= 20)
             {
-                overWeight = weight - 5;
-
+                overWeight = weight - 20;
                 price = ENTERPRISE_20KG + (overWeight * 100);
             }
             else
             {
                 price = ENTERPRISE_20KG;
             }
-            break;
-
-        case 4:
-            printf("Exiting...");
-            exit(1);
+            printf("El total a pagar es de: %.2f\n", price);
             break;
 
         default:
-            printf("Invalid option");
+            printf("Invalid option\n");
             break;
         }
 
-        choice = getUserChoice();
+        // Preguntar si desea continuar
+        printf("¿Desea continuar? (s/n): ");
+        scanf(" %c", &choice);
 
-    } while (choice != 'n' && choice != 'N')
+    } while (choice != 'n' && choice != 'N');
 }
+
+int main()
+{
+    shippingCalculator();
+    return 0;
+}
+
